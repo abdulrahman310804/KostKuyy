e5c4b1b02bd57d1e46b72d9ff6d6d6a7d14ea7be
abdulrahman310804 <abdulrhn66@gmail.com>
2024-10-14 22:16:08 +0700

    Update register_page.dart

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,1 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  String? _errorMessage;
+
+  Future<void> _register() async {
+    if (_usernameController.text.isEmpty || _passwordController.text.isEmpty) {
+      setState(() {
+        _errorMessage = 'Username dan password tidak boleh kosong';
+      });
+    } else {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString('username', _usernameController.text);
+      prefs.setString('password', _passwordController.text);
+      // ignore: use_build_context_synchronously
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+        backgroundColor: const Color.fromARGB(
+            255, 59, 247, 65), // Menambahkan warna hijau pada AppBar
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Tambahkan logo aplikasi
+            Image.asset(
+              'images/icon.png', // Pastikan jalur dan format file benar
+              width: 100,
+              height: 100,
+            ),
+            const SizedBox(height: 30),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Username',
+                border:
+                    OutlineInputBorder(), // Menambahkan border pada TextField
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border:
+                    OutlineInputBorder(), // Menambahkan border pada TextField
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _register,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(
+                    255, 59, 247, 65), // Ubah warna tombol menjadi hijau
+              ),
+              child: const Text('Register'),
+            ),
+            if (_errorMessage != null)
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text(
+                  _errorMessage!,
+                  style: const TextStyle(color: Colors.red),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
