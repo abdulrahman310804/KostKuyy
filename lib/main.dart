a1d1af783731ac2ea2572a753e534bff80ec2f5a
abdulrahman310804 <abdulrhn66@gmail.com>
2024-10-14 17:45:44 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:apk_kost/home_page.dart';
-import 'package:apk_kost/profile_page.dart';
-import 'package:apk_kost/saved_page.dart';
 import 'package:flutter/material.dart';
+import 'package:kostkuyy/profile_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'home_page.dart';
+import 'login_page.dart';
+import 'add_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,12 +15,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'apk_kost',
-      home: const HomePage(),
+      title: 'KostKuyy',
+      home: const AuthCheck(),
       routes: {
+        '/home': (context) => const HomePage(),
+        '/login': (context) => const LoginPage(),
+        '/add': (context) => const AddPage(),
         '/profile': (context) => const ProfilePage(),
-        '/saved': (context) => const SavedPage(),
       },
     );
+  }
+}
+
+class AuthCheck extends StatefulWidget {
+  const AuthCheck({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _AuthCheckState createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<AuthCheck> {
+  bool _isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? loggedIn = prefs.getBool('isLoggedIn');
+    setState(() {
+      _isLoggedIn = loggedIn ?? false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoggedIn) {
+      return const HomePage();
+    } else {
+      return const LoginPage();
+    }
   }
 }
